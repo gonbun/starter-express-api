dab55d3efc309d873f68ef5a58673e2eda5eabc9
Gonbun <gonbun@promised-land.info>
2023-01-29 16:10:29 +0900

    add

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const session = require("express-session");
+const { CyclicSessionStore } = require("@cyclic.sh/session-store");
+
 const app = express()
+const options = {
+    table: {
+      name: process.env.CYCLIC_DB,
+    }
+  };
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
