93b9d1ffc6649731ba12e537dabca33729040afd
Gonbun <gonbun@promised-land.info>
2023-01-29 16:13:41 +0900

    dix

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ const { CyclicSessionStore } = require("@cyclic.sh/session-store");
 
 const app = express()
 const options = {
-    table: {
-      name: process.env.CYCLIC_DB,
-    }
-  };
+  table: {
+    name: process.env.CYCLIC_DB,
+  },
+  keepExpired: false, 
+  touchInterval: 30000, // milliseconds (30 seconds)
+  ttl: 86400000 // milliseconds (1 day)
+};
+
+app.use(
+  session({
+    store: new CyclicSessionStore(options),
+    })
+);
 
 
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+  console.log(req.session.count)
+  console.log("Just got a request!")
+  if(!req.session.count){
+    req.session.count=0
+  }
+  req.session.count +=1
+  console.log(req.session.count)
+  res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
